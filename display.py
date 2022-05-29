ab20c688ea99d68f00d94ce70b996aa6a1c02236
joshuachio <jchio@gatech.edu>
2022-05-28 22:32:30 -0500

    added category pie chart

--- display.py
+++ display.py
@@ -1,10 +1,9 @@
-from cProfile import label
-from tkinter.font import families
 import streamlit as st
 from parse import Year, Month
 import datetime
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 # from data_collection import Get
 
 month_dict = {
@@ -46,6 +45,36 @@ if select == 'Yearly':
     with st.sidebar:
         year = st.selectbox("Choose a year", ("2022", "2023"))
     y = Year(year)
+    st.subheader(year + ' Overview')
+    c = pd.DataFrame([-y.total_debit, y.total_credit, y.net_gain], index=['Total Expenses', 'Total Income', 'Net Cash Flow'], columns=['Total ($)'])
+    s = px.bar(
+        c,
+        labels={'index': 'Categories', 'value': '($)'},
+        text_auto=True,
+    )
+    s.update_layout(
+        font_family='Serif',
+        font_size=14,
+        plot_bgcolor='#3A4356',
+        paper_bgcolor='#3A4356'
+    )
+    st.plotly_chart(s)
+    st.subheader("Category Breakdown")
+    cats_dict = y.categories
+    category_pie = go.Figure(
+        go.Pie(
+            labels=list(cats_dict.keys()),
+            values=list(cats_dict.values()),
+            hole=.58,
+            hoverinfo= 'label+percent',
+            textinfo='value'
+        )
+    )
+    category_pie.update_layout(
+        font_family='Serif',
+        font_size=14
+    )
+    st.plotly_chart(category_pie)
     st.subheader("Total Transactions for " + year)
     st.dataframe(y.budgetData.style.format({'amount': '{:.2f}'}).applymap(highlight, 'amount'), 800, 10000)
     st.subheader(year + ' ' + "Transactions Sorted by Category")
@@ -67,8 +96,28 @@ elif select == 'Monthly':
     s.update_layout(
         font_family='Serif',
         font_size=14,
+        plot_bgcolor='#3A4356',
+        paper_bgcolor='#3A4356'
     )
     st.plotly_chart(s)
+
+    st.subheader("Category Breakdown")
+    cats_dict = m.categories
+    category_pie = go.Figure(
+        go.Pie(
+            labels=list(cats_dict.keys()),
+            values=list(cats_dict.values()),
+            hole=.58,
+            hoverinfo= 'label+percent',
+            textinfo='value'
+        )
+    )
+    category_pie.update_layout(
+        font_family='Serif',
+        font_size=14
+    )
+    st.plotly_chart(category_pie)
+
     st.subheader("Total Transactions for " + month + " " + year)
     st.dataframe(m.transactions_df.style.format({'amount': '{:.2f}'}).applymap(highlight, 'amount'), 800, 10000)
     st.subheader(month + " " + year + ' ' + "Transactions Sorted by Category")
--- parse.py
+++ parse.py
@@ -9,19 +9,19 @@ class Month:
         self.month = month
         self.year = year
         self.budgetData = pd.read_excel('transactions.xlsx', year)
-        start_date = year + '-' + month + '-01'
-        if month == '04' or month == '06' or month == '09' or month == '11':
-            end_date = year + '-' + month + '-30'
-        elif month == '02':
-            if int(year) % 4 == 0:
-                end_date = year + '-' + month + '-29'
-            else:
-                end_date = year + '-' + month + '-28'
-        else:
-            end_date = year + '-' + month + '-31'
-
+        # start_date = year + '-' + month + '-01'
+        # if month == '04' or month == '06' or month == '09' or month == '11':
+        #     end_date = year + '-' + month + '-30'
+        # elif month == '02':
+        #     if int(year) % 4 == 0:
+        #         end_date = year + '-' + month + '-29'
+        #     else:
+        #         end_date = year + '-' + month + '-28'
+        # else:
+        #     end_date = year + '-' + month + '-31'
         # self.mask = (self.budgetData['date'] >= np.datetime64(start_date)) & (self.budgetData['date'] <= np.datetime64(end_date))
         # self.transactions_df = self.budgetData.loc[self.mask]
+
         self.transactions_df = self.budgetData[self.budgetData['date'].str.contains('-' + month + '-')]
         credit_df = self.transactions_df['amount'].where(self.transactions_df['amount'] > 0)
         self.total_credit = round(credit_df.sum(axis=0), 2)
@@ -30,6 +30,17 @@ class Month:
         self.net_gain = round(self.total_credit + self.total_debit, 2)
         self.category_transactions = self.transactions_df.sort_values(by=['category', 'amount'])
         self.price_sort = self.transactions_df.sort_values(by=['amount'])
+        self.categories = dict()
+        for i, v in enumerate(self.transactions_df['category']):
+            try:
+                if int(self.transactions_df['amount'][i]) > 0:
+                    continue
+                if v in self.categories:
+                    self.categories[v] -= self.transactions_df['amount'][i]
+                else:
+                    self.categories[v] = -self.transactions_df['amount'][i]
+            except:
+                continue
 
 class Year:
 
@@ -42,10 +53,20 @@ class Year:
         self.net_gain = round(self.total_credit + self.total_debit, 2)
         self.category_transactions = self.budgetData.sort_values(by=['category', 'amount'])
         self.price_sort = self.budgetData.sort_values(by=['amount'])
+        self.categories = dict()
+        for i, v in enumerate(self.budgetData['category']):
+            try:
+                if int(self.budgetData['amount'][i]) > 0:
+                    continue
+                if v in self.categories:
+                    self.categories[v] -= self.budgetData['amount'][i]
+                else:
+                    self.categories[v] = -self.budgetData['amount'][i]
+            except:
+                continue
 
-# chart_data = pd.DataFrame(
-#      [1, 2, 3],
-#      columns=["a", "b", "c"])
-c = pd.DataFrame([1, 2, 3], index=['first', 'second', 'third'], columns=['Total ($)'])
-# print(chart_data)
-print(c)
+# m = Month('03', '2022')
+# # print(23)
+# y = Month('05', '2022')
+# y = Month('04', '2022')
+# # print(m.transactions_df)
