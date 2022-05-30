a5a0b6a7b1ed8fc1baa1a1d8c2babeef178c1bb9
joshuachio <jchio@gatech.edu>
2022-05-30 16:43:47 -0500

    Optimized data collection

--- parse.py
+++ parse.py
@@ -8,7 +8,7 @@ class Month:
     def __init__(self, month: str, year: str):
         self.month = month
         self.year = year
-        self.budgetData = pd.read_excel('transactions.xlsx', year)
+        self.budgetData = pd.read_excel(year + '_transactions.xlsx', year)
         # start_date = year + '-' + month + '-01'
         # if month == '04' or month == '06' or month == '09' or month == '11':
         #     end_date = year + '-' + month + '-30'
@@ -46,7 +46,7 @@ class Month:
 class Year:
 
     def __init__(self, year: str):
-        self.budgetData = pd.read_excel('transactions.xlsx', year)
+        self.budgetData = pd.read_excel(year + '_transactions.xlsx', year)
         credit_df = self.budgetData['amount'].where(self.budgetData['amount'] > 0)
         self.total_credit = round(credit_df.sum(axis=0), 2)
         debit_df = self.budgetData['amount'].where(self.budgetData['amount'] < 0)
