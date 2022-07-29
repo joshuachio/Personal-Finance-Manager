d988f03cc1576bba6c0d5932ab3954e7dd0362f5
Joshua Chio <jchio@gatech.edu>
2022-07-29 13:29:34 -0500

    Moved files around

--- scripts/parse.py
+++ scripts/parse.py
@@ -9,18 +9,6 @@ class Month:
         self.month = month
         self.year = year
         self.budgetData = pd.read_excel(year + '_transactions.xlsx', year)
-        # start_date = year + '-' + month + '-01'
-        # if month == '04' or month == '06' or month == '09' or month == '11':
-        #     end_date = year + '-' + month + '-30'
-        # elif month == '02':
-        #     if int(year) % 4 == 0:
-        #         end_date = year + '-' + month + '-29'
-        #     else:
-        #         end_date = year + '-' + month + '-28'
-        # else:
-        #     end_date = year + '-' + month + '-31'
-        # self.mask = (self.budgetData['date'] >= np.datetime64(start_date)) & (self.budgetData['date'] <= np.datetime64(end_date))
-        # self.transactions_df = self.budgetData.loc[self.mask]
 
         self.transactions_df = self.budgetData[self.budgetData['date'].str.contains('-' + month + '-')]
         credit_df = self.transactions_df['amount'].where(self.transactions_df['amount'] > 0)
@@ -65,13 +53,3 @@ class Year:
                     self.categories[v] = -self.budgetData['amount'][i]
             except:
                 continue
-
-# m = Month('02', '2022')
-# # print(23)
-# y = Month('05', '2022')
-# y = Month('04', '2022')
-# may = Month('05', '2022')
-# # print(m.transactions_df['category'])
-# print(may.amount)
-# print(m.amount[0])
-# print(m.categories)
